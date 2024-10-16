4f7f581ab876749a2bcd01efbff5a3d903ab757f
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2024-10-15 17:14:53 -0700

    Update HomeBar.jsx

--- HomeBar.jsx
+++ HomeBar.jsx
@@ -1,6 +1,6 @@
 // Home.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; //which allows you to navigate to different routes in your application without reloading the page.
 
 const Home = ({ setShowSidebar }) => {
   const handleAdminClick = () => {
